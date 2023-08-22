08bb184b083f2f6df44c263cc19a8eacebef39db
daneker <kkraken2005@gmail.com>
2023-08-22 23:11:17 +0600

    ReducingDishes

--- src/leetcode/java/ReducingDishes.java
+++ src/leetcode/java/ReducingDishes.java
@@ -1,28 +1,18 @@
-package leetcode.utils.runner;
-
+package leetcode.java;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 
-public class CurrentRunner {
-    public static void main(String[] args) {
-
-    }
-
-    // [-1,-8,0,5,-9]
-    // -9 -8 -1 0 5
+public class ReducingDishes {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
 
-        int max = Integer.MIN_VALUE;
+        int max = 0;
 
         for (int i = 0; i < satisfaction.length; i++) {
             int sum = 0;
             int cook = 1;
             for (int j = i; j < satisfaction.length; j++) {
-                sum += satisfaction[i] * cook;
+                sum += satisfaction[j] * cook;
                 cook++;
             }
             max = Math.max(sum, max);
