8a354b83110297decd729ca90aca38b02fdb076d
daneker <kkraken2005@gmail.com>
2023-08-23 16:00:51 +0600

    group by

--- src/leetcode/utils/runner/CurrentRunner.java
+++ src/leetcode/utils/runner/CurrentRunner.java
@@ -1,34 +1,33 @@
 package leetcode.utils.runner;
 
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class CurrentRunner {
     public static void main(String[] args) {
-
+        HashMap<Integer, Integer> map = new HashMap<>();
     }
 
-    // [-1,-8,0,5,-9]
-    // -9 -8 -1 0 5
-    public int maxSatisfaction(int[] satisfaction) {
-        Arrays.sort(satisfaction);
 
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < satisfaction.length; i++) {
-            int sum = 0;
-            int cook = 1;
-            for (int j = i; j < satisfaction.length; j++) {
-                sum += satisfaction[i] * cook;
-                cook++;
+    public int[] canSeePersonsCount(int[] heights) {
+        int[] res = new int[heights.length];
+        for (int i = 0; i < heights.length - 1; i++) {
+            int currentCounter = 0;
+            int currentMin = 0;
+            for (int j = i + 1; j < heights.length; j++) {
+                res[i] = 0;
+                if (heights[j] > heights[i] || heights[j] < currentMin) {
+                    res[i] = currentCounter;
+                    break;
+                }
+                if (heights[j] < heights[i] && heights[j] > currentMin) {
+                    currentMin = heights[j];
+                    currentCounter++;
+                }
             }
-            max = Math.max(sum, max);
         }
 
-        return max;
-
+        return res;
     }
+
 }
